ff3298686d440d4dbdbf4f8ac8b8f2dc92eba83e
Rudraping4lerning <rudra@ping4learning.online>
2026-08-19 09:40:40 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
